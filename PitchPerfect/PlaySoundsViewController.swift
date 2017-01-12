79ed29191559abd073b3190d300f5ea68ad9662b
Aaron Lee <aarontlee10@berkeley.edu>
2017-01-12 15:52:44 -0800

    Updated UI and linked to model controller

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -6,18 +6,43 @@
 //  Copyright © 2017 Aarontlee. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class PlaySoundsViewController: UIViewController {
 
     var recordedAudioURL: URL!
     
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
